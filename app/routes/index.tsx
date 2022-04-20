4c2a373439cd8e4b5040f48e2c29c03cda344a59
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-04-20 23:04:35 +0100

    Added state for signed-in users

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,18 @@
-import { Link } from "@remix-run/react";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import { json, LoaderFunction } from "@remix-run/node";
+import { getUserId } from "~/utils/server/session.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await getUserId(request);
+  if (userId) return userId;
+  return json({});
+};
 
 export default function Index() {
+  const loaderData = useLoaderData();
+  let user;
+
+  typeof loaderData === "string" ? (user = loaderData) : (user = null);
   return (
     <div className="relative sm:pb-16 sm:pt-8">
       <div className="mx-auto max-w-[90rem] sm:px-6 lg:px-8 relative">
@@ -28,20 +40,39 @@ export default function Index() {
               file for instructions on how to get this project deployed.
             </p>
             <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-              <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                <Link
-                  to="/join"
-                  className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-                >
-                  Sign up
-                </Link>
-                <Link
-                  to="/login"
-                  className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600  "
-                >
-                  Log In
-                </Link>
-              </div>
+              {user ? (
+                <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                  <Link
+                    to="/notes"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
+                  >
+                    View Notes
+                  </Link>
+                  <Form action="/logout" method="post">
+                    <button
+                      type="submit"
+                      className="flex items-center w-full justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600"
+                    >
+                      Log Out
+                    </button>
+                  </Form>
+                </div>
+              ) : (
+                <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                  <Link
+                    to="/join"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
+                  >
+                    Sign up
+                  </Link>
+                  <Link
+                    to="/login"
+                    className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600  "
+                  >
+                    Log In
+                  </Link>
+                </div>
+              )}
               )
             </div>
             <a href="https://remix.run" className="mx-auto w-max">
