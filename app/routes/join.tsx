24a668013c5051f9ecce6f79916d8af4e769f239
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-04-20 23:04:54 +0100

    chore:Indexed extra status messages

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -6,6 +6,7 @@ import {
   useActionData,
   useLoaderData,
   useSearchParams,
+  useTransition,
 } from "@remix-run/react";
 import { LockClosedIcon } from "@heroicons/react/solid";
 import { createUserSession, getUserId } from "~/utils/server/session.server";
@@ -72,7 +73,7 @@ export const action: ActionFunction = async ({ request }) => {
     request,
     userId: user.id,
     remember: false,
-    redirectTo: typeof redirectTo === "string" ? redirectTo : "/",
+    redirectTo: typeof redirectTo === "string" ? redirectTo : "/notes",
   });
 };
 
@@ -86,7 +87,7 @@ export default function Join() {
   const [searchParams] = useSearchParams();
   const redirectTo = searchParams.get("redirectTo") ?? undefined;
   const actionData = useActionData() as ActionData;
-  const loaderData = useLoaderData();
+  const transition = useTransition();
   const emailRef = React.useRef<HTMLInputElement>(null);
   const passwordRef = React.useRef<HTMLInputElement>(null);
 
@@ -184,6 +185,43 @@ export default function Join() {
           </section>
         </div>
       </div>
+      {(transition.state == "submitting") && (
+        <div className="absolute bottom-10 md:bottom-14 flex flex-row justify-between content-center px-6 py-1 text-sm sm:text-base sm:px-7 sm:py-2 md:px-9 md:py-3 bg-blue-200 rounded-md border border-blue-600">
+          <p className="text-left relative top-1 ms:top-2 h-full flex content-center align-middle font-modern-sans text-lg sm:text-xl lg:text-2xl">
+            Crafting a new account..
+          </p>
+          <svg
+            className="animate-spin ml-3 h-5 w-5 md:w-7 md:h-7 text-blue-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          {/* <p className="text-gray-800 text-left">
+          Check the <a href="#">docs</a> for building a better optimistic UI
+        </p> */}
+        </div>
+      )}
+      {(transition.state == "submitting" || transition.state == "loading") && (
+        <div className="absolute bottom-4 md:bottom-7">
+          <p className="text-center text-gray-600 text-sm sm:text-base">
+            Check the docs for tips on better Optimistic UI and UX!
+          </p>
+        </div>
+      )}
     </div>
   );
 }
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
-import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
+import {
+  Form,
+  Link,
+  useActionData,
+  useSearchParams,
+  useTransition,
+} from "@remix-run/react";
 import { LoginIcon } from "@heroicons/react/outline";
 import { json, redirect } from "@remix-run/node";
 import { createUserSession, getUserId } from "~/utils/server/session.server";
@@ -80,6 +86,7 @@ export default function Login() {
   const [searchParams] = useSearchParams();
   const redirectTo = searchParams.get("redirectTo") || "/notes";
   const actionData = useActionData() as ActionData;
+  const transition = useTransition();
   const emailRef = React.useRef<HTMLInputElement>(null);
   const passwordRef = React.useRef<HTMLInputElement>(null);
 
@@ -92,7 +99,7 @@ export default function Login() {
   }, [actionData]);
 
   return (
-    <div className="h-screen w-screen self-center flex flex-row content-center justify-center align-middle m-auto sm:pb-16 pt-6 sm:pt-8">
+    <div className="h-screen w-screen self-center relative flex flex-row content-center justify-center align-middle m-auto sm:pb-16 pt-6 sm:pt-8">
       <div className="mx-auto max-w-3xl w-full sm:min-w-[31rem] md:min-w-[36rem] md:max-w-2xl lg:min-w-[48rem] px-4 sm:px-6 lg:px-8 relative self-center">
         <div className="relative shadow-xl overflow-hidden py-3 sm:py-5 rounded-lg sm:rounded-2xl lg:py-7">
           <section>
@@ -149,16 +156,12 @@ export default function Login() {
                 />
               </div>
               <div className="text-sm pt-1 text-red-700">
-              {actionData?.errors?.password && (
-                <div id="password-error">
-                  {actionData.errors.password}
-                </div>
-              )}
-              {actionData?.errors?.email && (
-                <div id="email-error">
-                  {actionData.errors.email}
-                </div>
-              )}
+                {actionData?.errors?.password && (
+                  <div id="password-error">{actionData.errors.password}</div>
+                )}
+                {actionData?.errors?.email && (
+                  <div id="email-error">{actionData.errors.email}</div>
+                )}
               </div>
             </div>
             <div className="flex flex-row mt-0 align-middle justify-center content-center px-8 md:px-12 lg:px-20">
@@ -179,6 +182,43 @@ export default function Login() {
           </Form>
         </div>
       </div>
+      {(transition.state == "submitting") && (
+        <div className="absolute bottom-10 md:bottom-14 flex flex-row justify-between content-center px-6 py-1 text-sm sm:text-base sm:px-7 sm:py-2 md:px-9 md:py-3 bg-blue-200 rounded-md border border-blue-600">
+          <p className="text-left relative top-1 ms:top-2 h-full flex content-center align-middle font-modern-sans text-lg sm:text-xl lg:text-2xl">
+            Hacking you in...
+          </p>
+          <svg
+            className="animate-spin ml-3 h-5 w-5 md:w-7 md:h-7 text-blue-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          {/* <p className="text-gray-800 text-left">
+          Check the <a href="#">docs</a> for building a better optimistic UI
+        </p> */}
+        </div>
+      )}
+      {(transition.state == "submitting") && (
+        <div className="absolute bottom-4 md:bottom-7">
+          <p className="text-center text-gray-600 text-sm sm:text-base">
+            Check the docs for tips on better Optimistic UI and UX!
+          </p>
+        </div>
+      )}
     </div>
   );
 }
