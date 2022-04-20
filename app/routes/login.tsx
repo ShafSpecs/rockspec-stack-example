11ba843ec9bf7e2e9f4d3195d6b2bb67475ac2b6
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-04-20 15:09:28 +0100

    Linked to Postgres db and started Optimistic UI

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,96 @@
-import { Link } from "@remix-run/react";
+import * as React from "react";
+import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 import { LoginIcon } from "@heroicons/react/outline";
+import { json, redirect } from "@remix-run/node";
+import { createUserSession, getUserId } from "~/utils/server/session.server";
+import { verifyLogin } from "~/models/user.server";
+import { validateEmail } from "~/utils/server/db-utils.server";
+
+import type {
+  ActionFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/node";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await getUserId(request);
+  if (userId) return redirect("/");
+  return json({});
+};
+
+interface ActionData {
+  errors?: {
+    email?: string;
+    password?: string;
+  };
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const redirectTo = formData.get("redirectTo");
+  const remember = formData.get("remember");
+
+  if (!validateEmail(email)) {
+    return json<ActionData>(
+      { errors: { email: "Email is invalid" } },
+      { status: 400 }
+    );
+  }
+
+  if (typeof password !== "string") {
+    return json<ActionData>(
+      { errors: { password: "Password is required" } },
+      { status: 400 }
+    );
+  }
+
+  if (password.length < 8) {
+    return json<ActionData>(
+      { errors: { password: "Password is too short" } },
+      { status: 400 }
+    );
+  }
+
+  const user = await verifyLogin(email, password);
+
+  if (!user) {
+    return json<ActionData>(
+      { errors: { email: "Invalid email or password" } },
+      { status: 400 }
+    );
+  }
+
+  return createUserSession({
+    request,
+    userId: user.id,
+    remember: remember === "on" ? true : false,
+    redirectTo: typeof redirectTo === "string" ? redirectTo : "/notes",
+  });
+};
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "Login",
+  };
+};
 
 export default function Login() {
+  const [searchParams] = useSearchParams();
+  const redirectTo = searchParams.get("redirectTo") || "/notes";
+  const actionData = useActionData() as ActionData;
+  const emailRef = React.useRef<HTMLInputElement>(null);
+  const passwordRef = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    if (actionData?.errors?.email) {
+      emailRef.current?.focus();
+    } else if (actionData?.errors?.password) {
+      passwordRef.current?.focus();
+    }
+  }, [actionData]);
+
   return (
     <div className="h-screen w-screen self-center flex flex-row content-center justify-center align-middle m-auto sm:pb-16 pt-6 sm:pt-8">
       <div className="mx-auto max-w-3xl w-full sm:min-w-[31rem] md:min-w-[36rem] md:max-w-2xl lg:min-w-[48rem] px-4 sm:px-6 lg:px-8 relative self-center">
@@ -13,7 +102,7 @@ export default function Login() {
               className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem] text-black"
             />
           </section>
-          <section className="mb-2 py-2">
+          <Form method="post" className="mb-2 py-2">
             <div>
               <h3 className="text-gray-500 text-center mt-4 md:mt-6 text-2xl md:text-3xl">
                 Sign in to your account
@@ -28,13 +117,14 @@ export default function Login() {
                 </Link>
               </p>
             </div>
-            <div className="rounded-md shadow-sm -space-y-px px-8 py-5 md:px-12 md:py-7 lg:px-20 lg:py-10">
+            <div className="rounded-md shadow-sm -space-y-px px-8 pt-5 pb-2 md:px-12 md:pt-7 md:pb-4 lg:px-20 lg:py-10">
               <div>
                 <label htmlFor="email-address" className="sr-only">
                   Email address
                 </label>
                 <input
                   id="email-address"
+                  ref={emailRef}
                   name="email"
                   type="email"
                   autoComplete="email"
@@ -51,14 +141,28 @@ export default function Login() {
                   id="password"
                   name="password"
                   type="password"
+                  ref={passwordRef}
                   autoComplete="current-password"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
                 />
               </div>
+              <div className="text-sm pt-1 text-red-700">
+              {actionData?.errors?.password && (
+                <div id="password-error">
+                  {actionData.errors.password}
+                </div>
+              )}
+              {actionData?.errors?.email && (
+                <div id="email-error">
+                  {actionData.errors.email}
+                </div>
+              )}
+              </div>
             </div>
             <div className="flex flex-row mt-0 align-middle justify-center content-center px-8 md:px-12 lg:px-20">
+              <input type="hidden" name="redirectTo" value={redirectTo} />
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
@@ -72,7 +176,7 @@ export default function Login() {
                 Sign In
               </button>
             </div>
-          </section>
+          </Form>
         </div>
       </div>
     </div>
