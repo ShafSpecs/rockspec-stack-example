4c49a641ff079cfe6632e6f6dd03b75b31fcf900
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-04-25 21:06:31 +0100

    chore: Adding direct flow to the db. CRUD wip

--- app/routes/notes/$noteId.tsx
+++ app/routes/notes/$noteId.tsx
@@ -1,12 +1,49 @@
-import type { LinksFunction } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import invariant from "tiny-invariant";
+import { deleteNote, getNote } from "~/models/notes.server";
+import { requireUserId } from "~/utils/server/session.server";
+
+import type {
+  LoaderFunction,
+  LinksFunction,
+  ActionFunction,
+} from "@remix-run/node";
+import type { Note } from "~/models/notes.server";
 
 import paper from "../../styles/paper.css";
+import { useLoaderData } from "@remix-run/react";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: paper }];
 };
 
+type LoaderData = {
+  note: Note;
+};
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const userId = await requireUserId(request);
+  invariant(params.noteId, "noteId not found");
+
+  const note = await getNote({ userId, id: params.noteId });
+  if (!note) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json<LoaderData>({ note });
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const userId = await requireUserId(request);
+  invariant(params.noteId, "noteId not found");
+
+  await deleteNote({ userId, id: params.noteId });
+
+  return redirect("/notes");
+};
+
 export default function NoteId() {
+  const data = useLoaderData() as LoaderData;
+
   return (
     <div className="mt-14 sm:mt-14 md:mt-16 lg:mt-18">
       <section className="paper h-[29rem] sm:h-[32rem] md:h-[34rem] lg:h-[38rem]">
@@ -15,7 +52,8 @@ export default function NoteId() {
             autoFocus
             draggable={false}
             placeholder="Hello world !&#10;Write notes here and save them for later."
-          ></textarea>
+            defaultValue={data.note.body}
+          />
         </div>
       </section>
       <section className="relative px-3 sm:px-0 mt-8 sm:mt-10 md:mt-12 lg:mt-14 content-center flex flex-col justify-center max-w-[768px] lg:max-w-[1024px] m-auto">
@@ -32,6 +70,7 @@ export default function NoteId() {
             name="title"
             id="title"
             placeholder="Give your note a title!"
+            defaultValue={data.note.title}
           />
         </div>
         <div className="w-full flex flex-row justify-end align-middle content-center">
