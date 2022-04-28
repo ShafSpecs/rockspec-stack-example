4d9168dbd83469a5a759359f0af7f3a4833f6aff
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-04-28 23:05:51 +0100

    Added a new font + aspect

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,14 @@ module.exports = {
         "128/85": "128 / 85",
         "128/70": "128 / 70",
         "128/55": "128 / 55",
+        // "650/459": "650 / 459",
+      },
+      boxShadow: {
+        "preview": "0 4px 30px rgba(0, 0, 0, 0.1)"
       }
+      // backgroundImage: {
+      //   "night-3": "url('/images/night_3.jpg')"
+      // }
     },
     fontFamily: {
       "mokoto-0": ['mokoto-0', 'sans-serif'],
@@ -17,6 +24,7 @@ module.exports = {
       "mokoto-3": ['mokoto-3', 'sans-serif'],
       "anxiety": ['Anxiety', 'sans-serif'],
       "modern-sans": ['Moderne Sans', 'sans-serif'],
+      "product-sans": ['Product Sans', 'sans-serif'],
     }
   },
   plugins: [
